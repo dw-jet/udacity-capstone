b1475ee67131bf0fbc249415f4c6df0e8c00e608
James Teitsworth <james@jamesteitsworth.com>
2020-06-09 13:23:30 -0500

    chore: Add starter files

---
+++ src/server/server.js
@@ -0,0 +1,37 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {data: []};
+
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+/* Middleware*/
+const bodyParser = require('body-parser');
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('dist'));
+
+
+// Setup Server
+const port = 3030;
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log(`Server running on port ${port}`);
+}
+
+app.get('/all', (req, res) => {
+  res.send(projectData);
+});
+
+app.post('/entry', (req, res) => {
+  projectData.data.push(req.body);
+  console.log(projectData);
+});
