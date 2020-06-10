ff7a7512ff578cca96a7336621717a8b52cb89ec
James Teitsworth <james@jamesteitsworth.com>
2020-06-10 16:02:07 -0500

    style: Use a more semantic name for section styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import { handleSubmit } from "./js/app";
 import './styles/resets.scss'
 import './styles/style.scss'
-import './styles/layout.scss'
+import './styles/sections.scss'
 
 export { handleSubmit }
