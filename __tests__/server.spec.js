ca2b805ad806d0350c2aa023d474e8d100a3e3f0
James Teitsworth <james@jamesteitsworth.com>
2020-06-11 16:05:35 -0500

    test: Get server test passing

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -3,6 +3,7 @@ const app = require('../src/server/server')
 const request = supertest(app)
 
 it('Server responds to get request', async done => {
-    const res = await request.get('/')
+    const res = await request.get('/');
     expect(res.status).toBe(200)
+    done();
 })
