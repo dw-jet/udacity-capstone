42744adce96358a44862b3b3ce00c4d0750bba5f
James Teitsworth <james@jamesteitsworth.com>
2020-06-11 21:52:03 -0500

    style: Beautify files and standardize spacing

--- src/server/constructWeatherAPILink.js
+++ src/server/constructWeatherAPILink.js
@@ -1,19 +1,18 @@
 const constructWeatherAPILink = (lat, lng, type) => {
-  const key = '&key=' + process.env.WEATHER_KEY;
-  const url = `http://api.weatherbit.io/v2.0/`
-  
-  const typeSegment = {
-    current: "current?units=I", 
-    forecast: "forecast/daily?units=I"
-  }
-  
-  const lat_long = `&lat=${lat}&lon=${lng}`
-  if (type == "current") {
-    return url + typeSegment.current + lat_long + key
-  } 
-  else {
-    return url + typeSegment.forecast + lat_long + key
-  }
+    const key = '&key=' + process.env.WEATHER_KEY;
+    const url = `http://api.weatherbit.io/v2.0/`
+
+    const typeSegment = {
+        current: "current?units=I",
+        forecast: "forecast/daily?units=I"
+    }
+
+    const lat_long = `&lat=${lat}&lon=${lng}`
+    if (type == "current") {
+        return url + typeSegment.current + lat_long + key
+    } else {
+        return url + typeSegment.forecast + lat_long + key
+    }
 }
 
 export { constructWeatherAPILink }
--- src/server/server.js
+++ src/server/server.js
@@ -24,7 +24,9 @@ const app = express();
 /* Middleware*/
 const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
@@ -35,31 +37,31 @@ app.use(cors());
 app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
-  res.sendFile('dist/index.html');
+    res.sendFile('dist/index.html');
 });
 
 app.get('/all', async (req, res) => {
-  const lat = projectData.geonames.lat;
-  const long = projectData.geonames.lng;
-  const name = encodeURI(`${projectData.geonames.name} ${projectData.geonames.state_region} landscape`);
-  const type = projectData.countdown < 8 ? "current" : "forecast";
-  const wbData = await getDataFromAPI(constructWeatherAPILink(lat, long, type));
-  projectData.weather = wbData.data;
-  
-  projectData.pixabay = await fetchPixabayData(name, projectData.geonames.country);
-  res.send(projectData);
+    const lat = projectData.geonames.lat;
+    const long = projectData.geonames.lng;
+    const name = encodeURI(`${projectData.geonames.name} ${projectData.geonames.state_region} landscape`);
+    const type = projectData.countdown < 8 ? 'current' : 'forecast';
+    const wbData = await getDataFromAPI(constructWeatherAPILink(lat, long, type));
+    projectData.weather = wbData.data;
+
+    projectData.pixabay = await fetchPixabayData(name, projectData.geonames.country);
+    res.send(projectData);
 });
 
 app.post('/geonames', (req, res) => {
-  const data = req.body;
-  projectData.countdown = data.countdown;
-  projectData.geonames = {
-    name: data.toponymName, 
-    lat: data.lat, 
-    lng: data.lng, 
-    state_region: data.adminName1, 
-    country: data.countryCode
-  };
+    const data = req.body;
+    projectData.countdown = data.countdown;
+    projectData.geonames = {
+        name: data.toponymName,
+        lat: data.lat,
+        lng: data.lng,
+        state_region: data.adminName1,
+        country: data.countryCode
+    };
 });
 
 module.exports = app
